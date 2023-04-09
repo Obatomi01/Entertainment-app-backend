0e2ae45b84eb29eca7cfac5064156d100f62fcac
Tomisin Eniola <eniolaoba9@gmail.com>
2023-04-09 22:03:57 +0100

    update!!!

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(userRoutes);
 // });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 8080;
